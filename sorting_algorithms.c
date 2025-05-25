12e7df61637cb9d2c7b0b5653b28a4a47ed60ccf
Gabriel Veleci <gabrielveleci@gmail.com>
2025-05-25 20:08:18 +0100

    algoritmo

--- sorting_algorithms.c
+++ sorting_algorithms.c
@@ -1,50 +1,5 @@
 #include "push_swap.h"
 
-void sort_stack(t_stack *a, t_stack *b)
-{
-
-
- while (a->size > 0)
- {
-    int top_a = a->top->value;
-    int next_a = a->top->next->value;
-    int bottow_a = a->top->prev->value;
-
-    int top_b = b->top ? b->top->value : 0;
-    int next_b = b->top ? b->top->next->value : 0;
-    int bottow_b = b->top ? b->top->prev->value : 0;
-
-    if(top_a > next_a)
-    {
-        sa(a);
-    }
-    else if (top_a > bottow_a)
-    {
-        rra(a);
-    }
-    else if (top_b < bottow_b)
-    {
-        rb(b);
-    }
-    else if (top_b < next_b)
-    {
-        sb(b);
-    }
-    else
-    {
-        pb(a, b);
-
-    }
- }
-    while (b->size > 0)
-    {
-        pa(a, b);
-    }
-
-}
-
-#include "push_swap.h"
-
 // Função para encontrar o maior número na stack
 int find_max(t_stack *stack) {
     int max = stack->top->value;
@@ -78,46 +33,65 @@ int calculate_average(int max, int min) {
     return (max + min) / 2;
 }
 
+// Função para calcular a média de todos os valores da stack
+int calculate_average_all(t_stack *stack) {
+    if (stack->size == 0)
+        return 0;
+    int sum = 0;
+    int count = 0;
+    t_node *current = stack->top;
+    while (count < stack->size) {
+        sum += current->value;
+        current = current->next;
+        count++;
+    }
+    return sum / count;
+}
+
+void push_below_average(t_stack *a, t_stack *b, int average) {
+    int size = a->size;
+    int i = 0;
+    while (i < size) {
+        if (a->top->value < average)
+            pb(a, b);
+        else
+            ra(a);
+        i++;
+    }
+}
+
+void sort_stack_a(t_stack *a) {
+    while (a->size > 0) {
+        int top = a->top->value;
+        int next = a->top->next->value;
+        int bottom = a->top->prev->value;
+        if (top > next)
+            sa(a);
+        else if (top > bottom)
+            rra(a);
+        else
+            break;
+    }
+}
+
+void sort_stack_b(t_stack *b) {
+    // Implemente se necessário
+}
+
+void move_back_to_a(t_stack *a, t_stack *b) {
+    while (b->size > 0)
+        pa(a, b);
+}
+
 // Atualização na função sort_stack
 void sort_stack(t_stack *a, t_stack *b) {
-    int max = find_max(a);
-    int min = find_min(a);
-    int average = calculate_average(max, min);
+    // int max = find_max(a);
+    // int min = find_min(a);
+    // int average = calculate_average(max, min);
+    int average = calculate_average_all(a);
 
-    // Passar todos os nós abaixo da média para a stack b
-    int size = a->size;
-    for (int i = 0; i < size; i++) {
-        if (a->top->value < average) {
-            pb(a, b);
-        } else {
-            ra(a); // Rotaciona para manter a ordem
-        }
-    }
-
-    // Continue com a lógica de ordenação
-    while (a->size > 0) {
-        int top_a = a->top->value;
-        int next_a = a->top->next->value;
-        int bottow_a = a->top->prev->value;
-
-        int top_b = b->top ? b->top->value : 0;
-        int next_b = b->top ? b->top->next->value : 0;
-        int bottow_b = b->top ? b->top->prev->value : 0;
-
-        if (top_a > next_a) {
-            sa(a);
-        } else if (top_a > bottow_a) {
-            rra(a);
-        } else if (top_b < bottow_b) {
-            rb(b);
-        } else if (top_b < next_b) {
-            sb(b);
-        } else {
-            pb(a, b);
-        }
-    }
-
-    while (b->size > 0) {
-        pa(a, b);
-    }
+    push_below_average(a, b, average);
+    sort_stack_a(a);
+    sort_stack_b(b);
+    move_back_to_a(a, b);
 }
